5ef0838f7119ee21e46512ff01ea474efcba9e50
TheTechFrog <96459640+TheSpiritOfDark@users.noreply.github.com>
2024-02-19 20:28:27 -0700

    Create Test.cs
    Actually put some effort in, u should read it

---
+++ Test.cs
@@ -0,0 +1,13 @@
+import system;
+//Crossystem reference????
+
+test = idfkatthispoint;
+
+if (test != ufuckedup){
+   print("yay");
+}
+else {
+   application.Quit();
+} 
+
+//i wrote this on mobile, dont attack me over it
